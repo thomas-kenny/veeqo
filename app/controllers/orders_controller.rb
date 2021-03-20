d94d87c1abee0e8afc91681536231aaf32575df9
Alex Marriott <marriottaog@gmail.com>
2021-03-20 14:54:38 +0000

    can now update order to mark as complete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
-
   def index
   end
 
   def show
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(packed: true)
+    redirect_to orders_path
   end
 end
